5383cf0f5c8854c78b0fd82b3226cec49bdcb6c9
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2022-06-27 21:10:25 -0300

    Added Bank1Account.get_balance

--- bank_account.py
+++ bank_account.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
+
+from banking_hub.packages.bank1 import Bank1AccountSource
 
 
 class BankAccount(ABC):
@@ -13,8 +16,17 @@ class BankAccount(ABC):
 
 
 class Bank1Account(BankAccount):
-    pass
 
+    def __init__(self, account_id) -> None:
+        self.account_id:int = account_id
+
+    def get_balance(self) -> Tuple[float, str]:
+        amount = Bank1AccountSource.get_account_balance(self.account_id)
+        currency = Bank1AccountSource.get_account_currency(self.account_id)
+        return amount, currency
+
+    def get_transactions(self):
+        pass
 
 class Bank2Account(BankAccount):
     pass
--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,6 +1,34 @@
-def test_get_account_balance():
-    pass
+from unittest import TestCase
+from unittest.mock import Mock, patch
 
 
-def test_get_transactions():
-    pass
+from banking_hub.bank_account import Bank1Account, BankAccount
+from banking_hub.packages.bank1 import Bank1AccountSource
+
+
+class TestBank1Account(TestCase):
+
+    @classmethod
+    def setUpClass(self) -> None:
+        self.bank_account: Bank1Account = Bank1Account(account_id=1234)
+        super().setUpClass()
+
+    @patch.object(Bank1AccountSource, 'get_account_currency')
+    @patch.object(Bank1AccountSource, 'get_account_balance')
+    def test_get_account_balance(
+        self,
+        mock_get_balance: Mock,
+        mock_get_currency: Mock
+    ):
+        mock_get_balance.return_value = 100
+        mock_get_currency.return_value = "BTC"
+        expected_balance = (100, "BTC")
+
+        balance = self.bank_account.get_balance()
+
+        self.assertEqual(balance, expected_balance)
+        mock_get_balance.assert_called_once_with(self.bank_account.account_id)
+        mock_get_currency.assert_called_once_with(self.bank_account.account_id)
+
+    def test_get_transactions(self):
+        pass
