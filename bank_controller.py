98f93368b0f88858e810c42ac9e351ec32cfca04
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2022-06-28 21:01:02 -0300

    Added BankController add_bank_account and remove_bank_account

--- bank_controller.py
+++ bank_controller.py
@@ -6,11 +6,15 @@ class BankController:
     def __init__(self) -> None:
         self.bank_accounts: dict[str, BankAccount] = {}
 
-    def add_bank_account(self):
-        pass
+    def add_bank_account(
+        self,
+        bank: str,
+        account: BankAccount
+    ) -> None:
+        self.bank_accounts[bank] = account
 
-    def remove_bank_account(self):
-        pass
+    def remove_bank_account(self, bank: str) -> None:
+        self.bank_accounts.pop(bank)
 
     def print_balances(self):
         pass
